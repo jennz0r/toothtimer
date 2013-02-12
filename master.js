6d9dc401fd1cac92cdee2cd561ffee859d0ecbdd
Brad H Ruderman <bradruderman@gmail.com>
2013-02-11 21:00:59 -0800

    updated js

--- master.js
+++ master.js
@@ -1,3 +1,38 @@
 function load(){
-	alert('hi!');
+	var videos = call_youtube();
+	timer();
+}
+
+function call_youtube(){
+	var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=cute+funny&type=video&videoDuration=short&key=AIzaSyAzbxEzpDORl5gUwugGG8z8IGcxRULSEMQ";
+	var videos = new Array();
+	$.get(url, function(data) {
+		for(i=0;i<data.items.length;i++)
+		{
+			videos.push(data.items[i].id.videoId);
+			//$('#temp').append("video id: " + data.items[i].id.videoId + "<br/>");
+		}
+		var randomnumber = Math.floor(Math.random()*videos.length);
+		//console.log(randomnumber);
+		var vHtml = "";
+		vhtml = "<iframe id=\"ytplayer\" type=\"text/html\" width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/{0}?autoplay=1\" frameborder=\"0\"/>";
+		$('#temp').append(vhtml.replace("{0}",videos[randomnumber]));
+	});
+}
+
+var count=120;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  $('.timer').html(count.toString());
 }
