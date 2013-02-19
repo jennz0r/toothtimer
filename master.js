d9752a2a72ea3e80ce3dbef76c05b9d0b25b9a71
jennz0r <jenn.ew@gmail.com [core]>
2013-02-18 17:18:38 -0800

    optimization

--- master.js
+++ master.js
@@ -1,27 +1,15 @@
 $(document).ready(function() {
+$('#button').on('click', call_youtube);
 
-$('#button').click(
+var count = 120;
+var counter = null;
+var timerid = $('#timer');
 
-	function call_youtube(){
-	
-	var count = 120;
-	var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
-	var thing = document.getElementById('timer');
-	function timer() {
-		count = count - 1;
-		if (count < 0) {
-    		clearInterval(counter);
-     		$('#timer').fadeOut(100, function(){
-     			$('#cheer').fadeIn(100);
-     		});
-     	return;
- 		}
-		$('#timer').html(count.toString());
-	}
-
+function call_youtube(){
 	var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=cute+funny&type=video&videoDuration=short&key=AIzaSyAzbxEzpDORl5gUwugGG8z8IGcxRULSEMQ";
-	var videos = new Array();
-
+	var videos = [];
+	var videoid = $('#video');
+	var buttonid = $('#button');
 	$.getJSON(url, function(data) {
 		for(i = 0; i < data.items.length; i++) {
 			videos.push(data.items[i].id.videoId);
@@ -30,13 +18,25 @@ $('#button').click(
 		var randomnumber = Math.floor(Math.random()*videos.length);
 		var vHtml = "";
 		vhtml = "<iframe id=\"ytplayer\" type=\"text/html\" width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/{0}?autoplay=1\" frameborder=\"0\"/>";
-		$('#video').append(vhtml.replace("{0}",videos[randomnumber]));
-		$('#video').fadeIn(100);
-		$('#button').hide();
-		$('#timer').show();
-		timer();
+		$(videoid).append(vhtml.replace("{0}",videos[randomnumber]));
+		$(videoid).fadeIn(100);
+		$(buttonid).hide();
+		$(timerid).show();
+		counter = setInterval(timer, 1000); //1000 will run it every 1 second
 	});
-
-});
+}
+	
+	function timer() {
+		var cheerid = $('#cheer');
+		count = count - 1;
+		if (count < 0) {
+    		clearInterval(counter);
+     		$(timerid).fadeOut(100, function(){
+     			$(cheerid).fadeIn(100);
+     		});
+     		return;
+ 		}
+		$(timerid).html(count.toString());
+	}
 
 });
